be6fc5fb0e107137def60a2c3e02dc5cfe525cee
prince-pansari <prince.pansari@byjus.com>
2022-08-17 21:17:08 +0530

    user token checked

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  NotAuthorized = Class.new(StandardError)
+  TokenMissing = Class.new(StandardError)
 
   rescue_from Apipie::ParamMissing, with: :handle_param_missing
   rescue_from Apipie::ParamInvalid, with: :handle_param_invalid
+  rescue_from ApplicationController::NotAuthorized, with: :user_not_authorized
+  rescue_from ApplicationController::TokenMissing, with: :token_missing
 
   private
 
@@ -18,6 +22,15 @@ class ApplicationController < ActionController::Base
       render(json: { error: { message: message, title: title } }, status: :unprocessable_entity) && return
     end
 
+    def user_not_authorized
+      message = 'User is not authorized'
+      render(json: { error: { message: message} }, status: :unauthorized) && return
+    end
+
+    def token_missing
+      message = 'Token Missing'
+      render(json: { error: { message: message} }, status: :unauthorized) && return
+    end
 
 end
 
