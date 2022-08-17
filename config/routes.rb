e30bec1a14432cc90e83d750f8ff2aa61ae85f83
prince-pansari <prince.pansari@byjus.com>
2022-08-17 17:36:24 +0530

    json issue resolved

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,19 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api do
-    resources :route, only: %i(show) do
+    resources :route, only: %i(show), format: "json" do
       get :get_all_routes, on: :collection
     end
-    resources :bus, only: %i() do
+    resources :bus, only: %i(), format: "json"  do
       get :get_bus_detail, on: :collection, to: 'bus#get_bus_detail'
     end
 
-    resources :ticket, only: %i() do
+    resources :ticket, only: %i(), format: "json" do
       post :book_ticket, on: :collection, to: 'ticket#book_ticket'
       get :get_tickets, on: :collection, to: 'ticket#get_tickets'
     end
 
-    resources :user, only: %i() do
+    resources :user, only: %i(), format: "json" do
       post :login, on: :collection, to: 'user#login'
       post :verify_otp, on: :collection, to: 'user#verify_otp'
     end
